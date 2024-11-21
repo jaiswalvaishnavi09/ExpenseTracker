3fc7c31f4101c4632a676405adfde0eb7f8082f1
Vaishnavi Jaiswal <126222719+jaiswalvaishnavi09@users.noreply.github.com>
2024-11-21 10:58:26 +0530

    Add files via upload

---
+++ DBConnect.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class DBConnect {
+   
+    public static Connection conn ;
+    public static Statement st ;
+    
+    static{
+        try{
+            conn =  DriverManager.getConnection("jdbc:mysql:///spendingdb"+"?useSSL=false", "root", "V@ishnavi09");
+            st = conn.createStatement();
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+        }
+    }
+}
